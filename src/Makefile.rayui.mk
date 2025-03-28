5dee803d5658b04c45247db339d957f6d4ddef4b
jfrey <nomhad@gmail.com>
2025-03-28 23:01:42 +0100

    moving raylib submodule

--- src/Makefile.rayui.mk
+++ src/Makefile.rayui.mk
@@ -8,17 +8,17 @@ RAYUI_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
 # adding required file for UI
 FILES_UI += \
 	$(RAYUI_PATH)/RayUI.cpp \
-	$(RAYUI_PATH)/../raylib/src/rcore.c \
-	$(RAYUI_PATH)/../raylib/src/rshapes.c \
-	$(RAYUI_PATH)/../raylib/src/rtext.c \
-	$(RAYUI_PATH)/../raylib/src/rtextures.c \
-	$(RAYUI_PATH)/../raylib/src/rmodels.c \
-	$(RAYUI_PATH)/../raylib/src/utils.c
+	$(RAYUI_PATH)/raylib/src/rcore.c \
+	$(RAYUI_PATH)/raylib/src/rshapes.c \
+	$(RAYUI_PATH)/raylib/src/rtext.c \
+	$(RAYUI_PATH)/raylib/src/rtextures.c \
+	$(RAYUI_PATH)/raylib/src/rmodels.c \
+	$(RAYUI_PATH)/raylib/src/utils.c
 
 # now the regular makefile for plugins (order matters)
 include $(RAYUI_PATH)/../dpf/Makefile.plugins.mk
 
 # Adding  custom libs
-BUILD_CXX_FLAGS += -I$(RAYUI_PATH) -I$(RAYUI_PATH)/../raylib/src -DPLATFORM_DPF
+BUILD_CXX_FLAGS += -I$(RAYUI_PATH) -I$(RAYUI_PATH)/raylib/src -DPLATFORM_DPF
 # for raylib
-BUILD_C_FLAGS += -I$(RAYUI_PATH)/../raylib/src -DPLATFORM_DPF -DGRAPHICS_API_OPENGL_ES2
+BUILD_C_FLAGS += -I$(RAYUI_PATH)/raylib/src -DPLATFORM_DPF -DGRAPHICS_API_OPENGL_ES2
